315a4bd6291f64abeff6cc98db1d2a7ca7a0ef91
Mateus Gutemberg <ytubegut@gmail.com>
2025-02-17 18:05:04 -0300

    added the sphere

--- cubo_aula.js
+++ cubo_aula.js
@@ -1,100 +1,84 @@
-
 var canvas;
 var gl;
 
-var NumVertices  = 36;
+var latitudeBands = 30;
+var longitudeBands = 30;
+var radius = 0.5;
 
-var vertices = [
-    vec3(0.5,   0.5,   0.5),//0
-    vec3(-0.5,  0.5,   0.5),//1
-    vec3(-0.5, -0.5,  0.5),//2
-    vec3(0.5,  -0.5,  0.5),//3
-    vec3(0.5,   0.5, -0.5),//4
-    vec3(-0.5,  0.5, -0.5),//5
-    vec3(-0.5, -0.5, -0.5),//6
-    vec3(0.5,  -0.5, -0.5) //7
-];
-var faces = [
-    [0, 1, 2, 3],
-    [4, 7, 6, 5],
-    [0, 4, 5, 1],
-    [1, 5, 6, 2],
-    [2, 6, 7, 3],
-    [3, 7, 4, 0]
-];
-var cores = [
-    vec3(1.0, 0.0, 0.0),
-    vec3(0.0, 1.0, 0.0),
-    vec3(0.0, 0.0, 1.0),
-    vec3(1.0, 1.0, 0.0),
-    vec3(1.0, 0.0, 1.0),
-    vec3(1.0, 1.0, 0.0)
-];
-var sh_colors = [];//36
-var sh_vertices =  [];//36
-function create_cube(){
-    for(var f = 0; f < faces.length; f++){
-        a = faces[f][0];
-        b = faces[f][1];
-        c = faces[f][2];
-        d = faces[f][3];
-        sh_vertices.push(vertices[a]);
-        sh_vertices.push(vertices[b]);
-        sh_vertices.push(vertices[c]);
-        sh_vertices.push(vertices[a]);
-        sh_vertices.push(vertices[c]);
-        sh_vertices.push(vertices[d]);
-        for(var c = 0; c < 6;  c++){
-            sh_colors.push(cores[f]);
+var sphereVertices = [];
+var sphereColors = [];
+var sphereIndices = [];
+
+function createSphere() {
+    for (var lat = 0; lat <= latitudeBands; lat++) {
+        var theta = lat * Math.PI / latitudeBands;
+        var sinTheta = Math.sin(theta);
+        var cosTheta = Math.cos(theta);
+
+        for (var lon = 0; lon <= longitudeBands; lon++) {
+            var phi = lon * 2 * Math.PI / longitudeBands;
+            var sinPhi = Math.sin(phi);
+            var cosPhi = Math.cos(phi);
+
+            var x = cosPhi * sinTheta;
+            var y = cosTheta;
+            var z = sinPhi * sinTheta;
+            
+            sphereVertices.push(vec3(radius * x, radius * y, radius * z));
+            sphereColors.push(vec3(0.5, 0.5, 0.5));
+        }
+    }
+
+    for (var lat = 0; lat < latitudeBands; lat++) {
+        for (var lon = 0; lon < longitudeBands; lon++) {
+            var first = (lat * (longitudeBands + 1)) + lon;
+            var second = first + longitudeBands + 1;
+
+            sphereIndices.push(first, second, first + 1);
+            sphereIndices.push(second, second + 1, first + 1);
         }
     }
 }
-window.onload = function init()
-{
-    canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-    
-    //criar o cubo
-    
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.1, 0.2, 0.2, 1.0);
     gl.enable(gl.DEPTH_TEST);
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    createSphere();
     
-    //  Linka e compila os shaders
-    var program = initShaders( gl, "vertex-shader", "fragment-shader");
-    gl.useProgram( program );
-    
-    create_cube();
-    //Vértices
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(sh_vertices), gl.STATIC_DRAW);    
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-    
-    //cor
-    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(sphereVertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
     var cBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(sh_colors), gl.STATIC_DRAW);    
-    
-    var vColor = gl.getAttribLocation( program, "vColor");
-    gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0, 0 );
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(sphereColors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-    //...
+
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(sphereIndices), gl.STATIC_DRAW);
+
     render();
-    //create_cube();
-}
+};
 
-function render()
-{
+function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, NumVertices);
-    requestAnimFrame( render );
+    gl.drawElements(gl.TRIANGLES, sphereIndices.length, gl.UNSIGNED_SHORT, 0);
+    requestAnimFrame(render);
 }
-
